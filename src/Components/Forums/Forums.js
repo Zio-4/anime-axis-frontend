a3dfcb6cb5c85f0bc73b0649bf4608d1ff947104
Philip Ziolkowski <philipz4848@gmail.com>
2021-10-10 17:47:12 -0700

    Rendering forum component and cards

--- src/Components/Forums/Forums.js
+++ src/Components/Forums/Forums.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import {useEffect, useState} from 'react'
 
 function Forums() {
+    const [forumPosts, setForumPosts] = useState([])
+
+    // Create custom method in backend for the 5 newest posts?
+
+    // useEffect(() => {
+    //     fetch("/forums_posts")
+    //     .then(r => r.json())
+    //     .then(forumPostData => {
+    //         console.log("Forum Post Data:", forumPostData)
+    //          setForumPosts(forumPostData)     
+    //     })
+    // }, [])
+
     return (
-        <div>
-            
+        <div className="forum-component">
+            <Container>
+                <header>
+                    <h1>Forums</h1>
+                </header>
+                    <Row>
+                        <Card >
+                            <Card.Header>Anime</Card.Header>
+                            <ListGroup variant="flush">
+                                    {/* <ListGroup.Item>{title} {content_summary} {posted}</ListGroup.Item>
+                                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item> */}
+                            </ListGroup>
+                        </Card>
+                    </Row>
+                    <Row>
+                        <Card >
+                            <Card.Header>Manga</Card.Header>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    </Row>
+                    <Row>
+                        <Card >
+                            <Card.Header>General</Card.Header>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    </Row>
+            </Container>
         </div>
     )
 }
