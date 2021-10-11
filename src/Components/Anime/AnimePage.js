dc5d852d8dca76ad46620f725828fd9cdb058a0f
Philip Ziolkowski <philipz4848@gmail.com>
2021-10-10 20:51:55 -0700

    Starting code on AnimePage

--- src/Components/Anime/AnimePage.js
+++ src/Components/Anime/AnimePage.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+import {useEffect, useState} from 'react'
 
 function AnimePage() {
+    const params = useParams()
+    const [anime, setAnime] = useState()
+
+
+    useEffect(() => {
+        fetch(`https://api.jikan.moe/v3/anime/${params.id}`)
+        .then(r => r.json())
+        .then(animeFetched => {
+            setAnime(animeFetched)
+        })
+    }, [])
+
+    console.log("Anime in state:", anime)
+
     return (
         <div>
             
